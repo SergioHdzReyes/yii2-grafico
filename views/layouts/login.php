59f5eebe99199d3e3b9eb04378cab1e1e9cf06f6
checo1990 <checoreyes_90290@hotmail.com>
2015-10-22 18:20:10 -0500

    Login

---
+++ views/layouts/login.php
@@ -0,0 +1,69 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Menu;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+use yii\helpers\Url;
+use yii\web\View;
+
+$this->registerCssFile('css/style.css');
+//$this->registerJsFile('/js/myscript.js', ['position'=> View::POS_END, 'depends'=>'yii\web\JqueryAsset']);
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <div class="menus">
+        <?php
+        NavBar::begin([
+            'brandLabel' => 'Acceso Grafico',
+            'brandUrl' => Yii::$app->homeUrl,
+            'renderInnerContainer'=>false,
+            'options' => [
+                'class' => 'navbar navbar-default',
+            ],
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'nav menu'],
+            'items' => [
+                ['label' => 'HOME', 'url' => ['/site/index']],
+                ['label' => 'NOSOTROS', 'url' => ['/site/about']],
+                ['label' => 'SERVICIOS', 'url' => ['/site/contact']],
+                ['label' => 'PORTAFOLIO', 'url' => ['/site/contact']],
+                ['label' => 'EQUIPO', 'url' => ['/site/contact']],
+                ['label' => 'PAQUETES', 'url' => ['/site/contact']],
+                ['label' => 'CONTACTO', 'url' => ['/site/contact']],
+            ],
+        ]);
+        NavBar::end();
+        ?>
+    </div>
+
+    <?= $content ?>
+</div>
+
+<footer class="piedepagina">
+    <p>Batallón de San Patricio 109, Valle Oriente San Pedro Garza García C.P. 66269  T: (81) 1934-2276 T: (81) 1968-050</p>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
--- views/site/error.php
+++ views/site/error.php
@@ -9,7 +9,7 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="container site-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
@@ -18,10 +18,10 @@ $this->title = $name;
     </div>
 
     <p>
-        The above error occurred while the Web server was processing your request.
+        El error anterior ha ocurrido mientras el servidor web procesaba tu peticion
     </p>
     <p>
-        Please contact us if you think this is a server error. Thank you.
+        Porfavor contactanos si piensas que es un error del servidor. Gracias.
     </p>
 
 </div>
